2d5f7ead1cd5588f10b8f5b85ebef9eabecf4d10
MURRAD-DESING <53040752+MURRAD-DESING@users.noreply.github.com>
2024-09-07 15:33:15 +0700

    Update home.php

--- home.php
+++ home.php
@@ -1,1 +1,39 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+    crossorigin="anonymous">
+    <script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar"
+    style="background-color: #f1beff;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">PORTAL BERITA</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Beranda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="kategori.php">Kategori</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="berita.php">Berita</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
